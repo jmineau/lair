8ca20ac223115176d5eeab89d4ac15b13127ad38
James Mineau <jameskmineau@gmail.com>
2023-06-13 13:59:37 -0600

    capitalize specie and better file reading

--- uataq/pipeline/instruments/bb.py
+++ uataq/pipeline/instruments/bb.py
@@ -24,27 +24,30 @@ INSTRUMENT = '2b'
 
 COL_MAPPER = {
     'FL2B': 'Flow_ccmin',
-    'OZNE': 'o3_ppb',
+    'OZNE': 'O3_ppb',
     'PS2B': 'Cavity_P_hPa',
     'TC2B': 'Cavity_T_C'}
-
-QC_LIMS = [0, 400]
 
 
 @preprocessor
 def get_files(site, lvl='raw', time_range=None, use_lin_group=False):
     if use_lin_group:
-        datetime_format = '%Y_%m_%d'
 
-        if site == 'trx01':
-            date_slicer = slice(3, 13)
-            dir_path = os.path.join(DATA_DIR, site, '2bo3', lvl)
-        elif site == 'trx02':
-            date_slicer = slice(10)
-            dir_path = os.path.join(DATA_DIR, site, '2bo3', lvl)
+        data_dir = os.path.join(DATA_DIR, site, '2bo3', lvl)
 
-        files_dates = [(os.path.join(dir_path, file), file[date_slicer])
-                       for file in os.listdir(dir_path)
+        if lvl == 'raw':
+            datetime_format = '%Y_%m_%d'
+
+            if site == 'trx01':
+                date_slicer = slice(3, 13)
+            elif site == 'trx02':
+                date_slicer = slice(10)
+        else:
+            datetime_format = '%Y_%m'
+            date_slicer = slice(7)
+
+        files_dates = [(os.path.join(data_dir, file), file[date_slicer])
+                       for file in os.listdir(data_dir)
                        if file.endswith('dat')]
 
     else:
@@ -64,14 +67,14 @@ def get_files(site, lvl='raw', time_range=None, use_lin_group=False):
 
 
 @preprocessor
-def read_obs(site, species=['o3'], lvl='raw', time_range=None,
+def read_obs(site, specie='O3', lvl='raw', time_range=None,
              use_lin_group=False):
-    assert all(specie in ('o3') for specie in species)
+    assert specie == 'O3'
 
     files = get_files(site, lvl=lvl, time_range=time_range,
                       use_lin_group=use_lin_group)
 
-    names = ['Time_UTC', 'o3_ppb', 'Cavity_T_C', 'Cavity_P_hPa', 'Flow_ccmin']
+    names = ['Time_UTC', 'O3_ppb', 'Cavity_T_C', 'Cavity_P_hPa', 'Flow_ccmin']
 
     dfs = []
     for file in files:
@@ -87,7 +90,7 @@ def read_obs(site, species=['o3'], lvl='raw', time_range=None,
             df['Time_UTC'] = pd.to_datetime(df.Time_UTC, errors='coerce',
                                             format='ISO8601')
 
-            for col in ['o3_ppb', 'Cavity_T_C', 'Cavity_P_hPa', 'Flow_ccmin']:
+            for col in names[1:]:
                 df[col] = pd.to_numeric(df[col], errors='coerce')
 
             # Drop rows where any column is missing data.
@@ -102,13 +105,15 @@ def read_obs(site, species=['o3'], lvl='raw', time_range=None,
             df = df.rename(columns=COL_MAPPER)
             df.replace(-9999, np.nan, inplace=True)
 
-        df = df.set_index('Time_UTC').sort_index()
         dfs.append(df)
 
     df = pd.concat(dfs)
 
-    df.where((df.o3_ppb > QC_LIMS[0]) & (df.o3_ppb < QC_LIMS[1]), inplace=True)
+    # Set time as index and filter to time_range
+    df = df.set_index('Time_UTC').sort_index()
+    df = df.loc[time_range[0]: time_range[1]]
 
+    # Drop horel cols that are not in names
     names_in_data = [name for name in names if name in df.columns]
 
-    return df.loc[time_range[0]: time_range[1], names_in_data]
+    return df.loc[:, names_in_data]
