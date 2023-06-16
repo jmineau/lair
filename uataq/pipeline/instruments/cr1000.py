2597727ed86e51a1a972e4683800bdaff3ff54a1
James Mineau <jameskmineau@gmail.com>
2023-06-15 20:46:56 -0600

    added cr1000 module

---
+++ uataq/pipeline/instruments/cr1000.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 17:27:03 2023
+
+@author: James Mineau (James.Mineau@utah.edu)
+
+Module of uataq pipeline functions for Horel-group's cr1000
+"""
+
+from functools import partial
+import pandas as pd
+
+from .. import horel
+from ..preprocess import preprocessor
+
+
+get_files = partial(horel.get_files, instrument='cr1000')
+
+
+@preprocessor
+def read_obs(site, specie='GPS', time_range=None):
+    files = get_files(site, time_range=time_range)
+
+    df = pd.concat([horel.read_file(file) for file in files])
+
+    return df
