e647319533e7f73fa446b799e9053f376f4abd5f
James Mineau <jameskmineau@gmail.com>
2024-07-23 13:56:20 -0600

    shared units registry with mass_flux context

--- lair/__init__.py
+++ lair/__init__.py
@@ -5,3 +5,18 @@ from . import utils
 
 
 __version__ = '2024.07.0'
+
+
+import cf_xarray.units  # must be imported before pint_xarray
+import pint
+import pint_xarray
+from pint_xarray import unit_registry as units
+
+mass_flux = pint.Context('mass_flux')
+mass_flux.add_transformation('[substance] / [area] / [time]',
+                             '[mass] / [area] / [time]',
+                        lambda units, substance, mw: substance * mw)
+mass_flux.add_transformation('[mass] / [area] / [time]',
+                             '[substance] / [area] / [time]',
+                        lambda units, mass, mw: mass / mw)
+units.add_context(mass_flux)
