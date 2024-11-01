c9d900cab8fd4f6fc1db75f904bf2447ef576288
James Mineau <jameskmineau@gmail.com>
2024-10-31 20:00:38 -0600

    SimulationCollection

--- lair/air/stilt.py
+++ lair/air/stilt.py
@@ -10,8 +10,9 @@ import subprocess
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from functools import cached_property
+import json
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 from typing_extensions import \
     Self  # requires python 3.11 to import from typing
 
@@ -140,12 +141,33 @@ class Receptor(metaclass=_UniqueReceptorMeta):
 
     def __init__(self, longitude: Any, latitude: Any, height: Any,
                  name: str | None = None):
-        self.longitude = longitude
-        self.latitude = latitude
-        self.height = height
+        self.longitude = float(longitude)
+        self.latitude = float(latitude)
+        self.height = float(height)
         self.name = name
 
         self.footprints = []  # ? keep track of footprints associated with receptor
+
+    @classmethod
+    def from_simulation_id(cls, sim_id: str) -> Self:
+        """
+        Extract receptor metadata from simulation ID.
+        Requires simulation ID to be in the format '%Y%m%d%H%M_long_lati_zagl'.
+
+        Parameters
+        ----------
+        sim_id : str
+            Simulation ID.
+
+        Returns
+        -------
+        Receptor
+            Receptor object.
+        """
+        parts = sim_id.split('_')
+        assert len(parts) == 4, 'Invalid simulation ID format'
+        run_time, long, lati, zagl = parts
+        return cls(longitude=long, latitude=lati, height=zagl)
 
     @property
     def id(self) -> str:
@@ -433,49 +455,39 @@ class Footprint(BaseGrid):
         return Footprint(simulation_id=self.simulation_id, run_time=self.run_time, receptor=self.receptor,
                          data=clipped, crs=self.crs, time_created=self.time_created)
 
-    def integrate_over_time_bins(self, t_bins):
-        """
-        Integrate footprint over time bins.
-
-        Parameters
-        ----------
-        t_bins : pd.IntervalIndex
-            Time bins for integration.
-
-        Returns
-        -------
-        xr.DataArray
-            Integrated footprint.
-        """
-        return self.data.groupby_bins(group='time', bins=t_bins, include_lowest=True).sum()
-
 
 class Simulation:
 
-    def __init__(self, path: str | Path,
-                 footprint_chunks: str | dict | None = None):
+    FAILURE_PHRASES = {
+        'Insufficient number of meteorological files found': 'MISSING_MET_FILES',
+        'meteorological data time interval varies': 'VARYING_MET_INTERVAL',
+    }
+
+    def __init__(self, path: str | Path):
         self.path = Path(path)
         assert self.path.exists(), f'Simulation not found: {self.path}'
+        self._path = self.path.resolve()
 
-        self.footprint_chunks = footprint_chunks
+        # Extract simulation ID from path
+        by_id_dir = Simulation.find_by_id_directory(self._path)
+        self.id = str(self._path.relative_to(by_id_dir))
 
-        by_id_dir = Simulation.find_by_id_directory(self.path)
-        self.id = str(self.path.relative_to(by_id_dir))
-
-        self.run_time = self.control.pop('run_time')
-        receptors = self.control['receptors']
-        self.receptor = receptors[0]  # TODO: support multiple receptors
-
-        self.input_path = self.path / f'{self.id}_input.json'
+        # Paths to output files
+        self.log_path = self.path / 'stilt.log'
+        self.config_path = self.path / f'{self.id}_config.json'
         self.traj_path = self.path / f'{self.id}_traj.parquet'
         self.foot_path = self.path / f'{self.id}_foot.nc'
+
+        # Get run time and receptor from simulation ID (requires default sim_id format)
+        self.run_time = dt.datetime.strptime(self.id.split('_')[0], '%Y%m%d%H%M')
+        self.receptor = Receptor.from_simulation_id(self.id)
 
     def __repr__(self) -> str:
         return f'Simulation({self.id})'
 
     @staticmethod
     def find_by_id_directory(path: Path) -> Path:
-        for parent in path.parents:
+        for parent in path.resolve().parents:
             if parent.name == 'by-id':
                 return parent
         raise FileNotFoundError('by-id directory not found in the path hierarchy')
@@ -528,14 +540,68 @@ class Simulation:
 
         return control
 
+    @cached_property
+    def log(self) -> str:
+        """
+        Read STILT log file.
+
+        Returns
+        -------
+        str
+            STILT log file contents.
+        """
+        if not self.log_path.exists():
+            raise FileNotFoundError(f'Log file not found: {self.log_path}')
+        return self.log_path.read_text()
+
+    @property
+    def was_successful(self) -> bool:
+        """
+        Check if simulation was successful by checking for the config json.
+
+        Returns
+        -------
+        bool
+            True if simulation was successful, False otherwise.
+        """
+        # TODO: is this the best way to do this?
+        # Other ideas include checking the stilt.log for keywords like 'STOP' or 'ERROR'
+        return self.config_path.exists()
+
+    def identify_failure_reason(self):
+        """
+        Identify reason for simulation failure.
+
+        Returns
+        -------
+        str
+            Reason for simulation failure.
+        """
+        if self.was_successful:
+            raise ValueError('Simulation was successful')
+
+        # Check log file for errors
+        for phrase, reason in self.FAILURE_PHRASES.items():
+            if phrase in self.log:
+                return reason
+            else:
+                return 'UNKNOWN'
+
+    @cached_property
+    def config(self) -> dict[str, Any]:
+        if not self.was_successful:
+            raise FileNotFoundError(f'Config file not found: {self.config_path}')
+        with self.config_path.open() as config_file:
+            return json.load(config_file)
+
     @property
     def has_trajectory(self) -> bool:
         return self.traj_path.exists()
 
     @cached_property
-    def trajectory(self) -> Trajectory | None:
+    def trajectory(self) -> Trajectory:
         if not self.has_trajectory:
-            return None
+            raise FileNotFoundError(f'Trajectory not found: {self.traj_path}')
         return Trajectory.from_parquet(self.traj_path)
 
     @property
@@ -543,64 +609,179 @@ class Simulation:
         return self.foot_path.exists()
 
     @cached_property
-    def footprint(self) -> Footprint | None:
+    def footprint(self) -> Footprint:
         if not self.has_footprint:
-            return None
-        foot = Footprint.from_netcdf(self.foot_path, chunks=self.footprint_chunks)
+            raise FileNotFoundError(f'Footprint not found: {self.foot_path}')
+        foot = Footprint.from_netcdf(self.foot_path)
         assert foot.run_time == self.run_time, f'Footprint run time {foot.run_time} does not match simulation run time {self.run_time}'
         assert foot.receptor == self.receptor, f'Footprint receptor {foot.receptor} does not match simulation receptor {self.receptor}'
         return foot
 
 
-class Model:
+class SimulationCollection(dict):
+    def __init__(self, out_dirs: str | Path | list[str | Path] | None = None):
+        super().__init__()  # initialize dict
+        self._failed = {}
+        self._successful = {}
 
-    def __init__(self, stilt_wd: str | Path,
-                 output_wd: str | Path | None = None,
-                 footprint_chunks: str | dict | None = None):
-        self.stilt_wd = Path(stilt_wd)
-        self.output_wd = Path(output_wd) if output_wd else self.stilt_wd / 'out'
-        self.footprint_chunks = footprint_chunks
+        if out_dirs is None:
+            self.out_dirs = []
+        elif isinstance(out_dirs, (str, Path)):
+            self.out_dirs = [Path(out_dirs)]
+        else:
+            self.out_dirs = [Path(d) for d in out_dirs]
+        self._load_simulations()
+
+    def __setitem__(self, key: str, sim: Simulation) -> None:
+        """
+        Set a simulation in the collection and categorize it.
+
+        Parameters
+        ----------
+        key : str
+            Simulation ID.
+        sim : Simulation
+            Simulation object.
+        """
+        super().__setitem__(key, sim)
+        if sim.was_successful:
+            self._successful[key] = sim
+        else:
+            self._failed[key] = sim
+
+    def _load_simulations(self) -> None:
+        for out_dir in self.out_dirs:
+            for log_file in out_dir.rglob('stilt.log'):
+                sim = Simulation(log_file.parent)
+                self[sim.id] = sim
 
     @property
-    def simulations(self) -> dict[str, Simulation]:
-        sims = {}
-        for control_file in (self.output_wd / 'by-id').rglob('CONTROL'):
-            sim = Simulation(control_file.parent, footprint_chunks=self.footprint_chunks)
-            sims[sim.id] = sim
-        return sims
+    def successful(self) -> dict[str, Simulation]:
+        return self._successful
+
+    @property
+    def failed(self) -> dict[str, Simulation]:
+        return self._failed
+
+    def get_missing(self, in_csv: str | Path, include_failed: bool = False) -> pd.DataFrame:
+        """
+        Find simulations in csv that are missing from output directory.
+
+        Parameters
+        ----------
+        in_csv : str | Path
+            Path to csv file containing simulation metadata.
+        include_failed : bool, optional
+            Include failed simulations in output. The default is False.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame of missing simulations.
+        """
+        # Load dataframes
+        in_df = pd.read_csv(in_csv)
+        sim_df = self.as_dataframe(subset='successful' if include_failed else None)
+
+        # Parse run_time as datetime
+        in_df['run_time'] = pd.to_datetime(in_df['run_time'])
+        sim_df['run_time'] = pd.to_datetime(sim_df['run_time'])
+
+        # Use run_time & receptor info to match simulations
+        cols = ['run_time', 'long', 'lati', 'zagl']
+
+        # Merge dataframes on run_time & receptor info
+        merged = pd.merge(in_df, sim_df[cols], on=cols, how='outer', indicator=True)
+        missing = merged[merged['_merge'] == 'left_only']
+        return missing.drop(columns='_merge')
+
+    def as_dataframe(self, subset: Literal['successful', 'failed'] | None = None) -> pd.DataFrame:
+        """
+        Convert simulation metadata to pandas DataFrame.
+
+        Parameters
+        ----------
+        subset : str, optional
+            Subset of simulations to include. The default is None.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame of simulation metadata.
+        """
+        if subset is None:
+            sims = self
+        else:
+            sims = {'successful': self._successful, 'failed': self._failed}[subset]
+
+        data = []
+        for sim in sims.values():
+            data.append({
+                'sim_id': sim.id,
+                'run_time': sim.run_time,
+                'long': sim.receptor.longitude,
+                'lati': sim.receptor.latitude,
+                'zagl': sim.receptor.height
+            })
+        return pd.DataFrame(data).set_index('sim_id').sort_values('run_time')
+
+    def to_csv(self, path: str | Path, subset: Literal['successful', 'failed'] | None = None) -> None:
+        """
+        Save simulation metadata to csv.
+        Contains columns for 'run_time', 'long', 'lati', and 'zagl'.
+
+        Returns
+        -------
+        None
+        """
+        df = self.as_dataframe(subset=subset)
+        df.to_csv(path, index=False)
+        return None
+
+
+class Model:
+
+    def __init__(self, path: str | Path,
+                 out_dirs: str | Path | list[str | Path] | None = None):
+        self.path = Path(path)
+        if isinstance(out_dirs, (str, Path)):
+            self.out_dirs = [Path(out_dirs)]
+        elif isinstance(out_dirs, list):
+            self.out_dirs = [Path(d) for d in out_dirs]
+        else:
+            self.out_dirs = [self.path / 'out']
+
+    @cached_property
+    def simulations(self) -> SimulationCollection:
+        return SimulationCollection(out_dirs=self.out_dirs)
 
     @property
     def run_times(self) -> set[dt.datetime]:
-        return {sim.run_time for sim in self.simulations.values()}
+        return {sim.run_time for sim in self.simulations.successful.values()}
 
     @property
     def receptors(self) -> set[Receptor]:
-        return {sim.receptor for sim in self.simulations.values()}
+        return {sim.receptor for sim in self.simulations.successful.values()}
 
-    # def footprints(self) -> Footprint | None:
-    #     footprints = []
-    #     for i, sim in enumerate(self.simulations.values()):
-    #         if i == 100:
-    #             break
-    #         if sim.has_footprint:
-    #             yield sim.footprint
-    #     if not footprints:
-    #         return None
-    #     return xr.merge(footprints)
-    
-    def footprints(self) -> Footprint | None:
-        foot_paths = [sim.foot_path for sim in self.simulations.values()
-                      if sim.has_footprint]
-        if not foot_paths:
-            return None
-        footprints = xr.open_mfdataset(foot_paths, preprocess=Footprint._preprocess,
-                                       combine='nested', concat_dim=['run_time', 'receptor'],
-                                       engine='h5netcdf', parallel=True, chunks='auto')
-        
-        return footprints
+    @property
+    def footprints(self) -> dict[str, Footprint]:
+        return {sim.id: sim.footprint
+                for sim in self.simulations.successful.values()
+                if sim.has_footprint}
 
-    def build_jacobian(self, obs_index, flux_index):
-        for sim_id, sim in self.simulations.items():
-            if sim.has_footprint:
-                foot = sim.footprint.sparse
-                foot.receptor.footprints.append(foot)
+    def failed_sims_by_reason(self) -> dict[str, list[Simulation]]:
+        """
+        Group failed simulations by reason for failure.
+
+        Returns
+        -------
+        dict
+            Dictionary of failed simulations grouped by reason.
+        """
+        reasons = {}
+        for sim in self.simulations.failed.values():
+            reason = sim.identify_failure_reason()
+            if reason not in reasons:
+                reasons[reason] = []
+            reasons[reason].append(sim)
+        return reasons
