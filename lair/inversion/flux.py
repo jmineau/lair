0afc6816e4d14ba47d641951e72da18d13fe8607
James Mineau <jameskmineau@gmail.com>
2025-06-21 00:10:26 -0600

    committing current changes to stilt - flux inversion framework before I try again

--- lair/inversion/flux.py
+++ lair/inversion/flux.py
@@ -31,6 +31,10 @@ from sklearn.metrics.pairwise import haversine_distances
 #   - polygonal flux "cells"
 # - multiple receptors
 
+# FIXME
+# - I don't know what the correct way to regrid footprints is / if there is a good way
+# - Perhaps I should just set the bounds and force everything to be on the same grid as the footprint?
+
 
 def generate_obs_index(receptor_obs: dict['Receptor', pd.Series]) -> pd.MultiIndex:
     """
@@ -162,6 +166,43 @@ def integrate_over_time_bins(data: xr.DataArray, time_bins: pd.IntervalIndex,
     return integrated
 
 
+def stack_obs_dims(data: xr.DataArray, obs_dims: list[str]) -> xr.DataArray:
+    """
+    Stack the observation dimensions into a single 'obs' dimension.
+
+    Parameters
+    ----------
+    data : xr.DataArray
+        Data to stack.
+    obs_dims : list[str]
+        Observation dimensions to stack.
+
+    Returns
+    -------
+    xr.DataArray
+        Stacked observations.
+    """
+    return data.stack(obs=obs_dims)
+
+
+def stack_flux_dims(data: xr.DataArray, flux_dims: list[str]) -> xr.DataArray:
+    """
+    Stack the flux dimensions into a single 'flux' dimension.
+
+    Parameters
+    ----------
+    data : xr.DataArray
+        Data to stack.
+    flux_dims : list[str]
+        Flux dimensions to stack.
+
+    Returns
+    -------
+    xr.DataArray
+        Stacked state.
+    """
+    return data.stack(flux=flux_dims)
+
 
 class CSVMixin(ABC):
     obs_dims: list[str]
@@ -183,7 +224,7 @@ class CSVMixin(ABC):
             Instance of CSVMixin.
         """
         data = pd.read_csv(path, index_col=cls.obs_dims)
-        return cls(data)
+        return cls(data)  # TODO: do i need to convert this to a series?
 
     def to_csv(self, path: str | Path) -> None:
         """
@@ -201,11 +242,19 @@ class ObsMixin(ABC):
     obs_dims: list[str] = ['receptor', 'obs_time']  # TODO maybe in the future this will be set by init
 
     def __init__(self, data: pd.Series):
-        assert data.index.names == self.obs_dims, f'index must have names: {self.obs_dims}'
+        self._check_obs_dims(data)
         data = data.rename(self.__class__.__name__.lower())  #  rename for consistency
         self.data = data
 
-    def stack_obs_dims(self, data: xr.DataArray) -> xr.DataArray:
+    def _check_obs_dims(self, data: pd.Series) -> None:
+        # Can be single index with just time or multiindex with receptor and time
+        if isinstance(data.index, pd.MultiIndex):
+            assert data.index.names == self.obs_dims, f'index must have names: {self.obs_dims}'
+        else:
+            assert data.index.name == self.obs_dims[-1], f'index must have name: {self.obs_dims[-1]}'
+
+    @classmethod
+    def stack_obs_dims(cls, data: xr.DataArray) -> xr.DataArray:
         """
         Stack the observation dimensions into a single 'obs' dimension.
 
@@ -219,7 +268,8 @@ class ObsMixin(ABC):
         xr.DataArray
             Stacked observations.
         """
-        return data.stack(obs=self.obs_dims)
+        obs_dims = [dim for dim in cls.obs_dims if dim in data.dims]
+        return stack_obs_dims(data, obs_dims=obs_dims)
 
 
 class Observation(ObsMixin, CSVMixin):
@@ -230,11 +280,54 @@ class Observation(ObsMixin, CSVMixin):
 
 
 class Background(ObsMixin, CSVMixin):
-    obs_dims: list[str] = ['obs_time']
 
     @property
     def c(self) -> xr.DataArray:
-        return self.data.to_xarray()
+        return self.stack_obs_dims(self.data.to_xarray())
+
+    def __add__(self, other: Self) -> Self:
+        """
+        Add two Background instances.
+
+        Parameters
+        ----------
+        other : Background
+            Another Background instance.
+
+        Returns
+        -------
+        Background
+            Sum of the two Background instances.
+        """
+        assert isinstance(other, Background), 'Can only add Background instances'
+        return self.__class__(self.data + other.data)
+
+    @classmethod
+    def from_flux(cls, flux: xr.DataArray, jacobian: 'Jacobian') -> Self:
+        """
+        Generate a constant background from a flux field.
+
+        Parameters
+        ----------
+        flux : xr.DataArray
+            Flux field. Must have one fewer dimension than the Jacobian matrix.
+        jacobian : Jacobian
+            Jacobian matrix.
+
+        Returns
+        -------
+        Background
+            Background data.
+        """
+        # Multiply the flux field by the Jacobian matrix
+        c = flux @ jacobian.data
+        c = c.to_series()  # convert to pandas series
+
+        # Create dynamic class based on the name of the flux array subclassing Background
+        name = ''.join([i.capitalize() for i in flux.name.split('_')])
+        cls_name = f'{name}Background'
+        cls = type(cls_name, (Background,), {})
+        return cls(c)
 
 
 class FluxMixin(ABC):
@@ -243,12 +336,17 @@ class FluxMixin(ABC):
     flux_dims: list[str] = ['flux_time', 'lat', 'lon']  # TODO maybe in the future this will be set by init
 
     def __init__(self, data: xr.DataArray):
+        self._check_flux_dims(data)
         self.data = data
 
         # Store regridder if it was used
         self._regridder = None
 
-    def stack_flux_dims(self, data: xr.DataArray) -> xr.DataArray:
+    def _check_flux_dims(self, data: xr.DataArray) -> None:
+        assert all(dim in data.dims for dim in self.flux_dims), f'data must have dimensions: {self.flux_dims}'
+
+    @classmethod
+    def stack_flux_dims(cls, data: xr.DataArray) -> xr.DataArray:
         """
         Stack the flux dimensions into a single 'flux' dimension.
 
@@ -262,7 +360,7 @@ class FluxMixin(ABC):
         xr.DataArray
             Stacked state.
         """
-        return data.stack(flux=self.flux_dims)
+        return stack_flux_dims(data, flux_dims=cls.flux_dims)
 
     @staticmethod
     def encode_flux_times(data: xr.Dataset) -> xr.Dataset:
@@ -441,11 +539,21 @@ class Jacobian(ObsMixin, FluxMixin):
         foot = integrate_over_time_bins(data=foot, time_bins=flux_times)
         foot = foot.rename({'time_bins': 'flux_time', 'run_time': 'obs_time'})
 
+        # FIXME THIS IS NOT HOW FOOTPRINTS SHOULD BE REGRIDDED
         # Regrid the footprint to the output grid
-        regridder = xe.Regridder(ds_in=foot, ds_out=out_grid, method='conservative',
-                                 weights=regrid_weights)
-        foot = regridder(foot, keep_attrs=True)
+        # regridder = xe.Regridder(ds_in=foot, ds_out=out_grid, method='conservative',
+        #                          weights=regrid_weights)
+        # foot = regridder(foot, keep_attrs=True)
         return foot
+
+    @classmethod
+    def from_path(cls, path):
+        jacobian = super().from_path(path)
+
+        # Nans can be introduced when combined jacobian files that were not computed together
+        # fill with 0  TODO: can I set the fillvalue to automatically do this?
+        jacobian.data = jacobian.data.fillna(0)
+        return jacobian
 
     @classmethod
     def from_stilt(cls, stilt: STILTModel, flux_times: pd.IntervalIndex,
@@ -475,15 +583,15 @@ class Jacobian(ObsMixin, FluxMixin):
             sim_df = sim_df[sim_df.run_time.dt.hour.isin(subset_hours)]
 
         # Compute the xesmf weights for regridding the footprints
-        if regrid_weights is not None and not Path(regrid_weights).exists():
-            print('Computing regrid weights...')
-            sim = stilt.simulations[sim_df['sim_id'].iloc[0]]
-            footprint = sim.footprint.clip_to_grid(out_grid, buffer=grid_buffer)
-            foot = footprint.data.to_dataset()
-            regridder = xe.Regridder(ds_in=foot, ds_out=out_grid, method='conservative')
-            regridder.to_netcdf(filename=regrid_weights)
-        else:
-            regridder = None
+        # if regrid_weights is not None and not Path(regrid_weights).exists():
+        #     print('Computing regrid weights...')
+        #     sim = stilt.simulations[sim_df['sim_id'].iloc[0]]
+        #     footprint = sim.footprint.clip_to_grid(out_grid, buffer=grid_buffer)
+        #     foot = footprint.data.to_dataset()
+        #     regridder = xe.Regridder(ds_in=foot, ds_out=out_grid, method='conservative')
+        #     regridder.to_netcdf(filename=regrid_weights)
+        # else:
+        regridder = None
 
         # Compute the Jacobian matrix in parallel
         H_rows = []
@@ -510,7 +618,7 @@ class Jacobian(ObsMixin, FluxMixin):
         del H.attrs['long_name']
         H.flux_time.attrs = {}  # drop flux_time attrs from stilt
 
-        empty_rows = H.isnull().all(dim=['flux_time', 'lat', 'lon'])  # TODO is there ever going to be empty rows?
+        empty_rows = H.isnull().all(dim=cls.flux_dims)  # TODO is there ever going to be empty rows?
         if empty_rows.any():
             print('Warning: Empty rows in Jacobian matrix for receptors: '
                   f'{empty_rows.where(empty_rows, drop=True).stack(receptor_time=("receptor", "obs_time")).receptor_time.values}')
@@ -547,7 +655,7 @@ class CovarianceMixin(ABC):
         CovarianceMixin
             Covariance matrix.
         """
-        return cls(np.load(path))
+        return cls(np.load(path))  # FIXME need to pass index somehow
 
     def to_npy(self, path: str | Path) -> None:
         """
@@ -560,7 +668,7 @@ class CovarianceMixin(ABC):
         """
         np.save(path, self.data)
 
-    def loc(self, index: pd.Index) -> np.ndarray:
+    def loc(self, index: pd.Index) -> Self:
         """
         Get the covariance matrix values for a given index.
 
@@ -571,11 +679,11 @@ class CovarianceMixin(ABC):
 
         Returns
         -------
-        np.ndarray
-            Covariance matrix values.
+        CovarianceMixin
+            Covariance matrix values for the given index
         """
         df = pd.DataFrame(self.data, index=self.index, columns=self.index)
-        return df.loc[index, index].values
+        return self.__class__(df.loc[index, index].values, index=index)
 
     def sort(self, index: pd.Index) -> Self:
         """
@@ -952,6 +1060,7 @@ class FluxInversion(Inversion):
                  grid_mask: xr.DataArray | None = None,
                  paths: dict[str, str | Path] | None = None,
                  ) -> None:
+        # TODO should probably allow setting obs_times/index from here?
         # Set project directory
         self.path = Path(project)
         self.project = self.path.name
@@ -980,24 +1089,26 @@ class FluxInversion(Inversion):
         self.jacobian = jacobian if jacobian is not None else self._load_input('jacobian')
         self.prior_error_cov = prior_error_cov if prior_error_cov is not None else self._load_input('prior_error_cov')
         self.modeldata_mismatch = modeldata_mismatch if modeldata_mismatch is not None else self._load_input('modeldata_mismatch')
+        
+        # Align indices
+        self.obs_index = None
+        self.flux_index = None
+        self._align_indices()
 
-        # Save original inputs
+        # Save aligned inputs
         self._save_inputs()
 
-        # Generate indices
-        self.obs_index = self.modeldata_mismatch.index
-        self.flux_index = generate_flux_index_from_latlon_grid(t_bins=flux_times, out_grid=out_grid, grid_mask=grid_mask)
-
+        # Get coordinates
         self.receptors = self.obs_index.get_level_values('receptor').unique()
         self.obs_times = self.obs_index.get_level_values('obs_time').unique()
         # self.flux_times = self.flux_index.get_level_values('flux_time').unique()
         self.lats = self.flux_index.get_level_values('lat').unique()
         self.lons = self.flux_index.get_level_values('lon').unique()
 
-        # Prepare inversion inputs
-        inputs = self._prepare_inputs()
-
-        super().__init__(**inputs)
+        # Initialize inversion
+        super().__init__(z=self.obs.z.values, c=self.background.c.values,
+                         x_0=self.prior.x_0.values, H=self.jacobian.H.values,
+                         S_0=self.prior_error_cov.data, S_z=self.modeldata_mismatch.data)
 
     def run(self):
         """
@@ -1031,25 +1142,25 @@ class FluxInversion(Inversion):
         """
         S_hat = super().S_hat
         return S_hat
+    
+    def _align_indices(self) -> None:
+        print('Aligning indices...')
+        # Align inputs
+        inputs = [self.obs.z, self.background.c, self.prior.x_0, self.jacobian.H]
+        aligned_inputs = xr.align(*inputs, join='inner')
 
-    def _prepare_inputs(self)-> dict[str, np.ndarray]:
-        """
-        Align the indices of the inputs.
-        """
-        inputs = {}
+        self.obs.data = aligned_inputs[0].to_pandas()
+        self.background.data = aligned_inputs[1].to_pandas()
+        self.prior.data = aligned_inputs[2].unstack()
+        self.jacobian.data = aligned_inputs[3].unstack()
 
-        print('Preparing inputs...')
-        obs_dim = xr.DataArray(self.obs_index, dims=['obs'])
-        flux_dim = xr.DataArray(self.flux_index, dims=['flux'])
-        
-        inputs['z'] = self.obs.z.reindex_like(obs_dim).values
-        inputs['c'] = self.background.c.sel(obs_time=self.obs_times).values
-        inputs['x_0'] = self.prior.x_0.reindex_like(flux_dim).values
-        inputs['H'] = self.jacobian.H.reindex_like(obs_dim).reindex_like(flux_dim).values
-        inputs['S_0'] = self.prior_error_cov.loc(self.flux_index).data
-        inputs['S_z'] = self.modeldata_mismatch.loc(self.obs_index).data
+        # Get new indices
+        self.obs_index = self.obs.data.index
+        self.flux_index = aligned_inputs[3].get_index('flux')
 
-        return inputs
+        # Align covariance matrices
+        self.prior_error_cov = self.prior_error_cov.loc(self.flux_index)
+        self.modeldata_mismatch = self.modeldata_mismatch.loc(self.obs_index)
 
     def _save_inputs(self):
         """Saves all provided inputs to individual files in the project directory."""
