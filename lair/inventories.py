2e945e45a23612755ad7362f32ec7c0e33923ea7
James Mineau <jameskmineau@gmail.com>
2026-06-23 00:28:36 -0600

    feat(inventories): support biweekly time_step in absolute_emissions

--- lair/inventories.py
+++ lair/inventories.py
@@ -287,6 +287,8 @@ class Inventory(BaseGrid):
         elif self.time_step == 'quarterly':
             t = time.to_index()  # days in each 3-month quarter starting at `time`
             seconds_per_step = ((t + pd.DateOffset(months=3)) - t).days.to_numpy() * 24 * 3600
+        elif self.time_step == 'biweekly':
+            seconds_per_step = np.full(time.size, 14 * 24 * 3600)
         elif self.time_step == 'daily':
             seconds_per_step = 24 * 3600
         elif self.time_step == 'hourly':
